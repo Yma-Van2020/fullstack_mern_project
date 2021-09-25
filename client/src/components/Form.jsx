d40bc68e15f12fc8ef01835fa5e2eb4871d14b4b
Yucong Ma <candicema2018@gmail.com>
2021-09-25 16:08:10 -0700

    update checkboxes

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,86 +1,94 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
-import { useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { analyzeAnsSentiment } from "../sentiment";
 
-const Form = ({userstate}) => {
-    const history = useHistory()
-    const [errorState, setErrorState] = useState({})
-    const [isChecked1, setIsChecked1] = useState(false);
-    const [isChecked2, setIsChecked2] = useState(false);
-    const [isChecked3, setIsChecked3] = useState(false);
-    const [formState, setFormState] = useState({
-        ans1:"",
-        ans2:"",
-        ans3:"",
-        ans4:"",
-        ans5:false,
-        ans6:false,
-        ans7:false,
-        dailyscore:0
-    })
+const Form = ({ userstate }) => {
+  const history = useHistory();
+  const [errorState, setErrorState] = useState({});
+  const [isChecked1, setIsChecked1] = useState(false);
+  const [isChecked2, setIsChecked2] = useState(false);
+  const [isChecked3, setIsChecked3] = useState(false);
+  const [formState, setFormState] = useState({
+    ans1: "",
+    ans2: "",
+    ans3: "",
+    ans4: "",
+    ans5: false,
+    ans6: false,
+    ans7: false,
+    dailyscore: 0,
+  });
 
-    const changeHandler = (e) => {
-        const { name, value } = e.target;
-        setFormState({
-          ...formState,
-          [name]: value,
-          dailyscore:analyzeAnsSentiment(formState.ans1,formState.ans2,formState.ans3,formState.ans4)
-        })
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newstate = { ...userstate };
+    newstate.moodScore.push(formState);
 
-      };
-    
-      const submitHandler = (e) => {
-        e.preventDefault();
-        const newstate = {...userstate}
-        newstate.moodScore.push(formState)
-      
-        axios
-          .post("http://localhost:8000/api/moods", formState, {withCredentials:true})
-          .then((res) => {
-            history.push("/result/" + res.data._id)
-          })
-          .catch((err) => {
-            const {errors} = err.response.data;
-            const errObj = {}
+    console.log(formState);
+    axios
+      .post("http://localhost:8000/api/moods", formState, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        history.push("/result/" + res.data._id);
+      })
+      .catch((err) => {
+        const { errors } = err.response.data;
+        const errObj = {};
 
-            for(const [key, value] of Object.entries(errors)){
-                // console.log(errors[key])
-                errObj[key] = value;
-            }
-            setErrorState(errObj)
-            console.log(errors)
-          });
-       
-      };
+        for (const [key, value] of Object.entries(errors)) {
+          // console.log(errors[key])
+          errObj[key] = value;
+        }
+        setErrorState(errObj);
+        console.log(errors);
+      });
+  };
 
-      const handleOnChange1 = (e) => {
-        setIsChecked1(!isChecked1);
-        const { name} = e.target;
-        setFormState({
-          ...formState,
-          [name]: !isChecked1,
-        });
-      };
-      const handleOnChange2 = (e) => {
-        setIsChecked2(!isChecked2);
-        const { name} = e.target;
-        setFormState({
-          ...formState,
-          [name]: !isChecked2,
-        });
-      };
-      const handleOnChange3 = (e) => {
-        setIsChecked3(!isChecked3);
-        const { name} = e.target;
-        setFormState({
-          ...formState,
-          [name]: !isChecked3,
-        });
-      };
+  const handleOnChange1 = (e) => {
+    setIsChecked1(!isChecked1);
+    const { name } = e.target;
+    setFormState({
+      ...formState,
+      [name]: !isChecked1,
+    });
+  };
+  const handleOnChange2 = (e) => {
+    setIsChecked2(!isChecked2);
+    const { name } = e.target;
+    setFormState({
+      ...formState,
+      [name]: !isChecked2,
+    });
+  };
+  const handleOnChange3 = (e) => {
+    setIsChecked3(!isChecked3);
+    const { name } = e.target;
+    setFormState({
+      ...formState,
+      [name]: !isChecked3,
+    });
+  };
 
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+      dailyscore: analyzeAnsSentiment(
+        formState.ans1,
+        formState.ans2,
+        formState.ans3,
+        formState.ans4,
+        formState.ans5,
+        formState.ans6,
+        formState.ans7
+      ),
+    });
+  };
 
   return (
     <div>
@@ -105,19 +113,21 @@ const Form = ({userstate}) => {
             </li>
 
             <li class="nav-item">
-               <Link class="nav-link" to="/form">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Fill out a form</span></Link>
+              <Link class="nav-link" to="/form">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Fill out a form</span>
+              </Link>
             </li>
 
             <li class="nav-item">
-               <Link class="nav-link" to="/pastforms">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Past Forms</span></Link>
+              <Link class="nav-link" to="/pastforms">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Past Forms</span>
+              </Link>
             </li>
 
             <li class="nav-item">
-            <Link class="nav-link" to="/calendar">
+              <Link class="nav-link" to="/calendar">
                 <i class="fas fa-fw fa-table"></i>
                 <span>Calender</span>
               </Link>
@@ -178,7 +188,6 @@ const Form = ({userstate}) => {
                       <span class="mr-2 d-none d-lg-inline text-gray-600 small">
                         {userstate.firstName}
                       </span>
-                    
                     </a>
 
                     <div
@@ -215,44 +224,135 @@ const Form = ({userstate}) => {
                     Please fill out the form below to analyze your mood score
                   </h1>
                 </div>
-                <div
-                 
-                >
-                
-                <form onSubmit={submitHandler} style={{padding:"40px 100px 100px 100px"}}>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Describe your current mood in one sentence</label>
-                    {(errorState.ans1)? <small className="ml-1 text-danger font-weight-bold">{errorState.ans1.message}</small>:null}
-                    <input onChange={changeHandler} type="text" class="form-control" name="ans1" id="exampleFormControlInput1" />
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Are you feeling optimistic?</label>
-                    {(errorState.ans2)? <small className="ml-1 text-danger font-weight-bold">{errorState.ans2.message}</small>:null}
-                    <input onChange={changeHandler} type="text" class="form-control" name="ans2" id="exampleFormControlInput1" />
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">What is your biggest accomplishment of the day?</label>
-                    {(errorState.ans3)? <small className="ml-1 text-danger font-weight-bold">{errorState.ans3.message}</small>:null}
-                    <input onChange={changeHandler} type="text" class="form-control" name="ans3" id="exampleFormControlInput1" />
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Have you been sociable?</label>
-                    {(errorState.ans4)? <small className="ml-1 text-danger font-weight-bold">{errorState.ans4.message}</small>:null}
-                    <input onChange={changeHandler} type="text" class="form-control" name="ans4" id="exampleFormControlInput1" />
-                </div>
-
-              <div class="form-group">
-          <label htmlFor="pl">Have you had little interest or pleasure in doing things?</label><br/>
-          <input onChange={handleOnChange1} className="form-check-input" type="checkbox" name="ans5" value={isChecked1} /><br/>
-          <label htmlFor="ep">Have you had trouble falling asleep, staying asleep, or have you been sleeping too much?</label><br/>
-          <input onChange={handleOnChange2} className="form-check-input" type="checkbox" name="ans6" value={isChecked2}/><br/>
-          <label htmlFor="hh">Have you been feeling bad about yourself – or feeling that you are a failure, or that you have let yourself or your family down?</label><br/>
-          <input onChange={handleOnChange3} className="form-check-input" type="checkbox"  name="ans7" id="hh" value={isChecked3}/><br/>
-          </div>
-                <input class="btn btn-secondary" type="submit" value="Submit" />&nbsp;&nbsp;
-                <Link to="/dashboard"><button class="btn btn-secondary">Go Back</button></Link>
-                </form>
-                  
+                <div>
+                  <form
+                    onSubmit={submitHandler}
+                    style={{ padding: "40px 100px 100px 100px" }}
+                  >
+                    <div class="form-group">
+                      <label htmlFor="pl">
+                        Have you had little interest or pleasure in doing
+                        things?
+                      </label>
+                      <br />
+                      <input
+                        checked={isChecked1}
+                        onChange={handleOnChange1}
+                        className="form-check-input"
+                        type="checkbox"
+                        name="ans5"
+                        value={isChecked1}
+                      />
+                      <br />
+                      <label htmlFor="ep">
+                        Have you had trouble falling asleep, staying asleep, or
+                        have you been sleeping too much?
+                      </label>
+                      <br />
+                      <input
+                        checked={isChecked2}
+                        onChange={handleOnChange2}
+                        className="form-check-input"
+                        type="checkbox"
+                        name="ans6"
+                        value={isChecked2}
+                      />
+                      <br />
+                      <label htmlFor="hh">
+                        Have you been feeling bad about yourself – or feeling
+                        that you are a failure, or that you have let yourself or
+                        your family down?
+                      </label>
+                      <br />
+                      <input
+                        checked={isChecked3}
+                        onChange={handleOnChange3}
+                        className="form-check-input"
+                        type="checkbox"
+                        name="ans7"
+                        id="hh"
+                        value={isChecked3}
+                      />
+                      <br />
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleFormControlInput1">
+                        Describe your current mood in one sentence
+                      </label>
+                      {errorState.ans1 ? (
+                        <small className="ml-1 text-danger font-weight-bold">
+                          {errorState.ans1.message}
+                        </small>
+                      ) : null}
+                      <input
+                        onChange={changeHandler}
+                        type="text"
+                        class="form-control"
+                        name="ans1"
+                        id="exampleFormControlInput1"
+                      />
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleFormControlInput1">
+                        Are you feeling optimistic?
+                      </label>
+                      {errorState.ans2 ? (
+                        <small className="ml-1 text-danger font-weight-bold">
+                          {errorState.ans2.message}
+                        </small>
+                      ) : null}
+                      <input
+                        onChange={changeHandler}
+                        type="text"
+                        class="form-control"
+                        name="ans2"
+                        id="exampleFormControlInput1"
+                      />
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleFormControlInput1">
+                        What is your biggest accomplishment of the day?
+                      </label>
+                      {errorState.ans3 ? (
+                        <small className="ml-1 text-danger font-weight-bold">
+                          {errorState.ans3.message}
+                        </small>
+                      ) : null}
+                      <input
+                        onChange={changeHandler}
+                        type="text"
+                        class="form-control"
+                        name="ans3"
+                        id="exampleFormControlInput1"
+                      />
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleFormControlInput1">
+                        Have you been sociable?
+                      </label>
+                      {errorState.ans4 ? (
+                        <small className="ml-1 text-danger font-weight-bold">
+                          {errorState.ans4.message}
+                        </small>
+                      ) : null}
+                      <input
+                        onChange={changeHandler}
+                        type="text"
+                        class="form-control"
+                        name="ans4"
+                        id="exampleFormControlInput1"
+                      />
+                    </div>
+                    <input
+                      class="btn btn-secondary"
+                      type="submit"
+                      value="Submit"
+                    />
+                    &nbsp;&nbsp;
+                    <Link to="/dashboard">
+                      <button class="btn btn-secondary">Go Back</button>
+                    </Link>
+                  </form>
                 </div>
 
                 <div class="row"></div>
--- client/src/sentiment.js
+++ client/src/sentiment.js
@@ -2,14 +2,27 @@ var Sentiment = require('sentiment');
 var sentiment = new Sentiment();
 
 module.exports = {
-  analyzeAnsSentiment (ans1, ans2, ans3, ans4) {
+  analyzeAnsSentiment (ans1, ans2, ans3, ans4, ans5, ans6, ans7) {
     let answer1 = sentiment.analyze(ans1).score;
     let answer2 = sentiment.analyze(ans2).score;
     let answer3 = sentiment.analyze(ans3).score;
     let answer4 = sentiment.analyze(ans4).score;
-    let answer5 = -2
-    let answer6 = -2
-    let answer7 = -2
+    let answer5 = 0
+    let answer6 = 0
+    let answer7 = 0
+    console.log("hi")
+    console.log(ans5)
+    console.log(ans6)
+    console.log(ans7)
+    if(ans5 == true){
+      answer5 = -2
+    }
+    if(ans6 == true){
+      answer6 = -2
+    }
+    if(ans7 == true){
+      answer7 = -2
+    }
 
     let sentimentScore = (answer1 + answer2 + answer3+ answer4 + answer5 + answer6 + answer7) 
     console.log(sentimentScore)
